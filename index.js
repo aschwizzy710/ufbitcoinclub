62cb0b2bc845d352c8b62fed1c6023703b9ee01e
aleczander <afigueroa81@ufl.edu>
2017-02-04 23:57:49 -0500

    fixed index added image header nav

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express');
-var server = express;
+var server = express();
+
+var port = process.env.PORT || 8080;
+server.use(express.static(__dirname + '/public'));
 
 server.get('/', function(request, response){
   response.sendFile('public/html/index.html', {root: __dirname});
